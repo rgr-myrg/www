d7196186a98d528871438b5758432c0eb2294d9b
hclair <harman.clair@teamspace.ca>
2013-12-18 12:32:07 -0400

    added config for iOS test rig

---
+++ json/ios-app-config.js
@@ -0,0 +1,15 @@
+[
+	{
+		"admsMeasureEnabled" : "true",
+		"admsReportSuiteIDs"   : "viarnd",
+		"omnitureChannel" : "Bento TestRig Channel",
+		"admsCharacterSet" : "ISO-8859-1",
+		"admsCurrencyCode" : "USD",
+		"admsSSLEnabled" : "false",
+		"admsLifecycleSessionTimeout" : "300",
+		"admsTrackingServer" : "sc.mtv.com",
+		"admsVisitorID" : "",
+		"admsxDebugLoggingEnabled" : "true",
+		"admsOfflineTrackingEnabled" : "true",
+	}
+]
