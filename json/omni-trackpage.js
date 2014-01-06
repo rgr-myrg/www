4ee39e540778d4996256e854299a90a771b72ebb
HC <harman.clair@theredspace.com>
2014-01-06 12:29:14 -0400

    adjustments to remote file

--- json/omni-trackpage.js
+++ json/omni-trackpage.js
@@ -1,8 +1,8 @@
 [
 	{
 		"pageName" : "dev/bento/remote/testrig",
-		"linkName" : "dev/bento/remote/link",
 		"prop1"    : "remote value of prop 1",
-		"eVar1"    : "remote value of evar 1"
+		"eVar1"    : "remote value of evar 1",
+		"channel"  : "remote sandbox channel",
 	}
 ]
