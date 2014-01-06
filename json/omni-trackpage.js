911d4fd2f6ed819bb36a68cd58c08551af49ca25
HC <harman.clair@theredspace.com>
2014-01-06 12:43:44 -0400

    bug fix

--- json/omni-trackpage.js
+++ json/omni-trackpage.js
@@ -3,6 +3,6 @@
 		"pageName" : "dev/bento/remote/testrig",
 		"prop1"    : "remote value of prop 1",
 		"eVar1"    : "remote value of evar 1",
-		"channel"  : "remote sandbox channel",
+		"channel"  : "remote sandbox channel"
 	}
 ]
