43746a9ca36d1b76d76d40622915827e774117d4
HC <harman.clair@theredspace.com>
2014-02-25 14:11:45 -0400

    more ad remote configs

---
+++ json/ios-ads-other.js
@@ -0,0 +1,15 @@
+[
+	{
+		"x" : "0",
+		"y"   : "0",
+		"width" : "300",
+		"height" : "250",
+		"appName" : "btg_test_app",
+		"sections" : "",
+		"isDFP : "false",
+		"enableCloseButton" : "true",
+		"closeButtonInterval" : "5",
+		"isRefreshable : "false",
+		"refreshInterval" : "10"
+	}
+]
