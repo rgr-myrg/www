af4bb57c6f798d45a210edb6eeb2168aa8624a3c
HC <harman.clair@theredspace.com>
2014-02-28 13:37:54 -0400

    string bools -> bools

--- json/ios-app-config.js
+++ json/ios-app-config.js
@@ -1,14 +1,14 @@
 [
 	{
-		"admsMeasureEnabled" : "true",
+		"admsMeasureEnabled" : true,
 		"admsReportSuiteIDs"   : "viarnd",
 		"admsCharacterSet" : "ISO-8859-1",
 		"admsCurrencyCode" : "USD",
-		"admsSSLEnabled" : "false",
-		"admsLifecycleSessionTimeout" : "300",
+		"admsSSLEnabled" : false,
+		"admsLifecycleSessionTimeout" : 300,
 		"admsTrackingServer" : "sc.mtv.com",
 		"admsVisitorID" : "",
-		"admsxDebugLoggingEnabled" : "true",
-		"admsOfflineTrackingEnabled" : "true"
+		"admsxDebugLoggingEnabled" : true,
+		"admsOfflineTrackingEnabled" : true
 	}
 ]
