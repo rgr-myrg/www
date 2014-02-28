f9970df0ad95ca6239afa92e2212bd524b7308cc
HC <harman.clair@theredspace.com>
2014-02-28 13:43:56 -0400

    typo

--- json/ios-app-config.js
+++ json/ios-app-config.js
@@ -4,7 +4,7 @@
 		"admsReportSuiteIDs"   : "viarnd",
 		"admsCharacterSet" : "ISO-8859-1",
 		"admsCurrencyCode" : "USD",
-		"admsSSLEnabled" : false,
+		"admsSslEnabled" : false,
 		"admsLifecycleSessionTimeout" : 300,
 		"admsTrackingServer" : "sc.mtv.com",
 		"admsVisitorID" : "",
