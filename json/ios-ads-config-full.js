6a247fa0151edcc1c4bb31bbe53e8e4784f76d64
HC <harman.clair@theredspace.com>
2014-03-13 14:53:18 -0300

    ads component test redesign

---
+++ json/ios-ads-config-full.js
@@ -0,0 +1,41 @@
+[
+	{
+        @"config" : {
+			"isCoppaCompliant" : true
+        },
+        @"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 300,
+			"height" : 250,
+			"appName" : "btg_test_app",
+			"sections" : "",
+			"isDFP" : false,
+			"enableCloseButton" : true,
+			"closeButtonInterval" : 5,
+			"isRefreshable" : false,
+			"refreshInterval" : 10,
+			"keyValues" : {
+				"!category" : "v1.2.3",
+				"ver"   : "v1.2.3",
+				"testmodePrefix" : "celtra_mraid"
+			},
+			"keychainKeyValues" : {
+				"aamSegments" : ""
+			},
+			"dfpData" : {
+				"metadata" : {
+					"property" : "mtv_news",
+					"hub" : "breaking",
+					"is_main" : true
+				},
+				"mappings" : {
+					"ad-unit" : {
+						"1" : "@property",
+						"2" : "@hub"
+					}
+				}		        	
+			}
+        }
+    }
+]
