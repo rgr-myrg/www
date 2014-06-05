df86eb596538d3224a794a74e909d4529fab43bc
HC <harman.clair@theredspace.com>
2014-06-05 14:55:48 -0300

    testing namespaces

--- json/omni-tracklink.js
+++ json/omni-tracklink.js
@@ -2,5 +2,9 @@
 	{
 		"linkName" : "dev/bento/remote/link",
 		"prop1"    : "remote value of prop 1",
+		"contextData"   : {
+            		"m.contextDataKey1"    : "testcontextvalue1",
+            		"contextDataKey2"    : "testcontextvalue2"
+        	}
 	}
 ]
--- json/omni-trackpage.js
+++ json/omni-trackpage.js
@@ -1,8 +1,24 @@
 [
 	{
 		"pageName" : "dev/bento/remote/testrig",
+		"channel"  : "remote sandbox channel",
 		"prop1"    : "remote value of prop 1",
-		"eVar1"    : "remote value of evar 1",
-		"channel"  : "remote sandbox channel"
+        "prop11"    : "remote value of prop 11",
+        "prop74"    : "remote value of prop 74",
+        "prop75"    : "remote value of prop 75",
+        "prop76"    : "remote value of prop 76",
+        "prop111"    : "remote value of prop 111",
+        "eVar1"    : "remote value of evar 1",
+        "eVar75"    : "remote value of evar 75",
+        "eVar76"    : "remote value of evar 76",
+        "eVar74"    : "remote value of evar 74",
+        "eVar111"    : "remote value of evar 111",
+        "eVar300"    : "remote value of evar 300",
+        "channel"  : "remote sandbox channel",
+        "contextData"   : {
+            "m.contextDataKey1"    : "testcontextvalue1",
+            "contextDataKey2"    : "testcontextvalue2"
+        }
 	}
 ]
+
