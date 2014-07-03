4d9152c6439c39f46055f053bbb74d41e0a8bce6
HC <harman.clair@theredspace.com>
2014-07-03 11:33:52 -0300

    tve enabled off file

---
+++ json/ios-tve-config-off.js
@@ -0,0 +1,19 @@
+[
+	{
+		"admsMeasureEnabled" : true,
+		"admsReportSuiteIDs"   : "viarnd",
+		"tveEnabled" : false,
+		"tveContextData" : {
+			"TVEContextDataSignInStartPageName" : "test_rig_sign_in_start",
+			"TVEContextDataProviderLoadedPageName" : "test_rig_provider_loaded",
+			"TVEContextDataSignOnCompletedPageName" : "test_rig_sign_on_completed",
+			"TVEContextDataProviderNotListedClickedPageName" : "test_rig_provider_not_listed",
+			"TVEContextDataElvisPickedMethodPageName" : "test_rig_elvis_picked_method",
+			"TVEContextDataElvisSucceedMethodPageName" : "test_rig_elvis_succeed_method",
+			"TVEContextDataElvisHasJustExpiredPageName" : "test_rig_elvis_preview_has_just_expired",
+			"TVEContextDataElvisPreviewLockedPageName" : "test_rig_elvis_locked",
+			"TVEContextDataActionPropOrigin" : "test_rig_origin",
+			"channel" : "test_rig_channel"
+		}
+	}
+]
