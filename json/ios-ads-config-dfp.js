0c9dbd45299dffb96317ac4924ce75b6376181fa
HC <harman.clair@theredspace.com>
2014-07-23 15:37:26 -0300

    dip true

---
+++ json/ios-ads-config-dfp.js
@@ -0,0 +1,39 @@
+[
+	{
+		"config" : {
+			"isCoppaCompliant" : true
+		},
+		"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 300,
+			"height" : 250,
+			"appName" : "btg_test_app",
+			"sections" : "",
+			"isDFP" : true,
+			"enableCloseButton" : true,
+			"closeButtonInterval" : 5,
+			"isRefreshable" : false,
+			"refreshInterval" : 10,
+			"keyValues" : {
+				"testmodePrefix" : "celtra_mraid"
+			},
+			"keychainKeyValues" : {
+				"aamSegments" : ""
+			},
+			"dfpData" : {
+				"metadata" : {
+					"property" : "mtv_news",
+					"hub" : "breaking",
+					"is_main" : true
+				},
+				"mappings" : {
+					"ad-unit" : {
+						"1" : "@property",
+						"2" : "@hub"
+					}
+				}		        	
+			}
+		}
+	}
+]
