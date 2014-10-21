f23903d85ce60d01f736415f0281624ffcb6bd6f
HC <harman.clair@redspace.com>
2014-10-21 14:39:04 -0300

    com score enabled crash test

---
+++ json/ios-adms-appConfig-contextData.js
@@ -0,0 +1,21 @@
+[
+	{
+		"admsMeasureEnabled" : true,
+		"admsReportSuiteIDs"   : "viarnd",
+		"admsCharacterSet" : "ISO-8859-1",
+		"admsCurrencyCode" : "USD",
+		"admsSslEnabled" : true,
+		"admsLifecycleSessionTimeout" : 300,
+		"admsTrackingServer" : "sc.mtv.com",
+		"admsVisitorID" : "testVID",
+		"admsDebugLoggingEnabled" : true,
+		"admsOfflineTrackingEnabled" : true,
+		"audienceManagerEnabled" : true,
+		"comScoreEnabled" : true,
+		"admsContextData"   : {
+			"contextDataKey1onconfig"    : "testcontextvalue1onconfig",
+			"contextDataKey2onconfig"    : "testcontextvalue2onconfig",
+			"contextDataKey1trackpage"    : "testcontextvalue1onconfig"
+		}
+	}
+]
