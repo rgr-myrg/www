d2181532b12fa3591ab02a93880afa9c11e9227c
HC <harman.clair@redspace.com>
2015-01-12 12:49:21 -0400

    track link page name

--- json/omni-tracklink.js
+++ json/omni-tracklink.js
@@ -2,6 +2,7 @@
 	{
 		"linkName" : "dev/bento/remote/link",
 		"prop1"    : "remote value of prop 1",
+		"pageName" : "omni-tracklink-test-page-name",
 		"contextData"   : {
             		"m.contextDataKey1"    : "testcontextvalue1",
             		"contextDataKey2"    : "testcontextvalue2",
