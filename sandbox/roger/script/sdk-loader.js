46263d6a735ad11495a64b111c20c9e7b8906ecd
Mayorga, Roger <roger.mayorga@cbsi.com>
2018-03-23 13:38:55 -0400

    Added bulk load with retry callback

--- sandbox/roger/script/sdk-loader.js
+++ sandbox/roger/script/sdk-loader.js
@@ -17,7 +17,6 @@
 				scriptType      = 'text/javascript',
 				scriptAsync     = true,
 				scriptOnReady   = Function.prototype,
-				scriptOnError   = Function.prototype,
 				statusComplete  = {status: 'complete'},
 				statusError     = {status: 'error'},
 				totalRetryCount = 0,
@@ -28,13 +27,32 @@
 				return scriptElement || document.createElement('script');
 			}.bind(this);
 
-			var notifyError = function(e) {
-				statusError.error = e;
-				scriptOnError.call(this, statusError);
+			var getScriptInfo = function() {
+				return {
+					src: scriptSrc,
+					type: scriptType,
+					async: scriptAsync
+				};
+			}.bind(this);
+
+			var notifyStatus = function() {
+				status = arguments[0];
+
+				if (arguments.length > 0) {
+					status.error = arguments[1];
+				}
+
+				status.info = getScriptInfo();
+				scriptOnReady.call(this, status);
 			}.bind(this);
 
 			this.src = function(src) {
-				scriptSrc = src.replace(/^http:|^https:/gi, '');
+				scriptSrc = src;
+
+				if (typeof src === 'string') {
+					scriptSrc = src.replace(/^http:|^https:/gi, '');
+				}
+
 				return this;
 			};
 
@@ -58,24 +76,24 @@
 				return this;
 			};
 
-			this.onReady = function(callback) {
+			this.ready = function(callback) {
 				scriptOnReady = callback;
-				return this;
-			};
-
-			this.onError = function(callback) {
-				scriptOnError = callback;
 				return this;
 			};
 
 			this.load = function() {
 				if (scriptSrc === null) {
-					notifyError('src is missing');
+					notifyStatus(statusError, 'src is missing');
+					return;
+				}
+
+				if (typeof scriptSrc !== 'string' && scriptSrc.length > 0) {
+					this.bulkLoad(scriptSrc);
 					return;
 				}
 
 				if (History.has(scriptSrc)) {
-					scriptOnReady.call(this, statusComplete);
+					notifyStatus(statusComplete);
 					return;
 				}
 
@@ -87,22 +105,19 @@
 
 				script.onreadystatechange = script.onload = function() {
 					if (!script.readyState || /loaded|complete/.test(script.readyState)) {
-						script.onreadystatechange = script.onload = null;
-
+						scriptElement = script.onreadystatechange = script.onload = null;
 						History.add(scriptSrc);
-						scriptOnReady.call(this, statusComplete);
-						scriptElement = null;
+						notifyStatus(statusComplete);
 					}
 				}.bind(this);
 
 				script.onerror = function(e) {
-					totalRetryCount++;
-
 					/* Remove failed script from the DOM */
 					document.querySelector('head').removeChild(script);
 
+					totalRetryCount++;
+
 					if (totalRetryCount <= maxNumOfRetries) {
-						console.log('[retry]', totalRetryCount);
 						setTimeout(
 							function() { this.load(); }.bind(this),
 							timeToWait
@@ -110,28 +125,43 @@
 						return;
 					}
 
-					notifyError(e);
+					notifyStatus(statusError, e);
 				}.bind(this);
 
 				try {
 					document.querySelector('head').appendChild(script);
 				} catch (e) {
-					notifyError(e);
+					notifyStatus(statusError, e);
 				}
 			};
 
 			this.bulkLoad = function(list) {
-				list.forEach(function(src) {
+				var scripts  = list,
+					results  = [],
+					onready  = scriptOnReady;
+
+				var callback = function(result) {
+					results.push(result);
+
+					if (scripts.length === 0) {
+						onready.call(this, results);
+					} else {
+						loadNext();
+					}
+				}.bind(this);
+
+				var loadNext = function() {
 					obj.SdkLoader.script()
-						.src(src)
+						.src(scripts.shift())
 						.type(scriptType)
 						.async(scriptAsync)
 						.retry(maxNumOfRetries)
 						.delay(timeToWait)
-						.onReady(scriptOnReady)
-						.onError(scriptOnError)
+						.ready(callback)
 						.load();
-				});
+				}.bind(this);
+
+				loadNext();
 			};
 
 			return this;
@@ -148,26 +178,22 @@ uvpjs.SdkLoader.script()
 	.async(true)
 	.retry(3)
 	.delay(2000)
-	.onReady((result) => {
-		console.log(result);
-	})
-	.onError((result) => {
+	.ready((result) => {
 		console.log(result);
 	})
 	.load();
 
 uvpjs.SdkLoader.script()
+	.src([
+		'script/uvpjs-builder.js',
+		'script/uvpc-config.js',
+		'script/uvpc-comscore.js',
+		'http://rgr-myrg.github.io/www/sandbox/build/lib/tracking/mux.js'
+	])
 	.async(true)
 	.retry(5)
 	.delay(2000)
-	.onReady((result) => {
-		console.log(result);
+	.ready((result) => {
+		console.log('ready', result);
 	})
-	.onError((result) => {
-		console.log(result);
-	})
-	.bulkLoad([
-		'script/uvpjs-builder.js',
-		'script/uvpc-config.js',
-		'script/uvpc-comscore.js'
-	]);
+	.load();
