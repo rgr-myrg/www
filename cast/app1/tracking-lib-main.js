3dc91bf2fa073c2863de87adf814af3c6b152d71
27352 <solidity@gmx.com>
2019-03-22 15:45:09 -0400

    js fix 7

--- cast/app1/tracking-lib-main.js
+++ cast/app1/tracking-lib-main.js
@@ -39,32 +39,32 @@ tracker.setContextData({
     visitorId: '<your-visitor-id>'
 });
 
-// tracker.onCastEvent(event => {
-//     switch (event.type) {
-//         case cast.events.EventType.LOAD_START:
-//             this.track('sessionStart', {
-//                 playerInitTime: (new Date()).getTime(),
-//                 playerName: '<your-player-name>',
-//                 playerVersion: '<your-player-version>',
-//                 sessionId: '<your-session-id>',
-//                 videoElement: document.getElementById('castMediaElementId')
-//             });
-//             break;
-//
-//         case cast.events.EventType.CLIP_STARTED:
-//             this.track('contentStart', {
-//                 assetURL: 'cbs.com/shows',
-//                 duration: 5000,
-//                 mediaId: 'aaEb-cF09-0g17-r63t',
-//                 videoTitle: 'CBS Sports - Superbowl',
-//                 seriesTitle: 'Superbowl',
-//                 isLive: false,
-//                 episodeFlag: false
-//             });
-//             break;
-//     }
-// });
-
 tracker.onCastEvent(event => {
-    console.log('[#####]', event);
+    switch (event.type) {
+        case cast.events.EventType.LOAD_START:
+            tracker.track('sessionStart', {
+                playerInitTime: (new Date()).getTime(),
+                playerName: '<your-player-name>',
+                playerVersion: '<your-player-version>',
+                sessionId: '<your-session-id>',
+                videoElement: document.getElementById('castMediaElementId')
+            });
+            break;
+
+        case cast.events.EventType.CLIP_STARTED:
+            tracker.track('contentStart', {
+                assetURL: 'cbs.com/shows',
+                duration: 5000,
+                mediaId: 'aaEb-cF09-0g17-r63t',
+                videoTitle: 'CBS Sports - Superbowl',
+                seriesTitle: 'Superbowl',
+                isLive: false,
+                episodeFlag: false
+            });
+            break;
+    }
 });
+
+// tracker.onCastEvent(event => {
+//     console.log('[#####]', event);
+// });
