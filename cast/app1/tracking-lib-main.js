91da55ffef6aed5e2ee7c02b1249b9394ecb3c5c
27352 <solidity@gmx.com>
2019-03-22 10:45:38 -0400

    Implementing new chromecast tracker

---
+++ cast/app1/tracking-lib-main.js
@@ -0,0 +1,66 @@
+/*
+ * Create an instance of the ChromecastReceiver.
+ *
+ * The ChromecastReceiver will retrieve the CastReceiverContext and Cast Player Manager from CAF.
+ * This allows the Tracking API to attach event listeners to Chromecast events.
+ * The CastReceiverContext and Player Manager can be accessed through the Tracking API
+ * tracking.context and tracking.playerManager respectively if needed.
+ */
+
+ let tracker = new vtg.tracking.ChromecastTracker();
+ tracker.setDebug(true);
+
+ tracker.setConfig({
+    Mux: {
+        enabled: true,
+        params: {
+            experimentName: 'AllAccess Chromecast Player',
+            propertyKey: 'ec83cce4c209447a2af3d62f2',
+            subPropertyId: 'AU-AllAccess'
+        }
+    },
+    Adobe: {
+        enabled: true,
+        params: {
+            prodApiServer: 'cbs-stage.hb-api.omtrdc.net',
+            devApiServer: 'cbs-dev.hb-api.omtrdc.net',
+            trackingServer: '<your-aa-tracking-server>',
+            reportSuite: '<your-aa-rsid>',
+            marketingCloudOrgId: '<your-aa-marketingCloudOrgId>',
+            channel: 'AU-AllAccess',
+            enableSSL: true
+        }
+    }
+});
+
+tracker.setContextData({
+    brandPlatformId: '<your-brand-platform-id>',
+    userId: '<your-user-id>',
+    visitorId: '<your-visitor-id>'
+});
+
+tracker.onCastEvent(event => {
+    switch (event.type) {
+        case cast.events.EventType.LOAD_START:
+            this.track('sessionStart', {
+                playerInitTime: (new Date()).getTime(),
+                playerName: '<your-player-name>',
+                playerVersion: '<your-player-version>',
+                sessionId: '<your-session-id>',
+                videoElement: document.getElementById('castMediaElementId')
+            });
+            break;
+
+        case cast.events.EventType.CLIP_STARTED:
+            this.track('contentStart', {
+                assetURL: 'cbs.com/shows',
+                duration: 5000,
+                mediaId: 'aaEb-cF09-0g17-r63t',
+                videoTitle: 'CBS Sports - Superbowl',
+                seriesTitle: 'Superbowl',
+                isLive: false,
+                episodeFlag: false
+            });
+            break;
+    }
+});
